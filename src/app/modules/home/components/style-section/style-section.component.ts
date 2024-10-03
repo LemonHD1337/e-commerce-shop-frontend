fa6961a2d04fdb291f51814007387247cd292e50
patryk <patryk.pujanek@wp.pl>
2024-10-03 23:25:44 +0200

    added section with search by dress style

---
+++ src/app/modules/home/components/style-section/style-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-style-section',
+  templateUrl: './style-section.component.html',
+  styleUrl: './style-section.component.scss'
+})
+export class StyleSectionComponent {
+
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,9 +5,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { BanerComponent } from './components/baner/baner.component';
 import { BrandSliderComponent } from './components/brand-slider/brand-slider.component';
+import { StyleSectionComponent } from './components/style-section/style-section.component';
 
 @NgModule({
-  declarations: [HomeComponent, BanerComponent, BrandSliderComponent],
+  declarations: [HomeComponent, BanerComponent, BrandSliderComponent, StyleSectionComponent],
   imports: [CommonModule, HomeRoutingModule],
   exports: [HomeComponent]
 })
