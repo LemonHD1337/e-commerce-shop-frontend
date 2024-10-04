eae6cab3a2c20faf35345860402ca90a7fb15d16
patryk <patryk.pujanek@wp.pl>
2024-10-04 13:18:23 +0200

    corrected routerLink to path with query params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { CoreModule } from './modules/core/core.module';
 import { HomeModule } from './modules/home/home.module';
+import { ProductModule } from './modules/product/product.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HomeModule, ProductModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
