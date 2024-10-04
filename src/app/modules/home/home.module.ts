787654f0f49554ca37ad7e3033df9e3e736821db
patryk <patryk.pujanek@wp.pl>
2024-10-04 08:37:45 +0200

    added product card component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,10 +6,11 @@ import { HomeComponent } from './components/home/home.component';
 import { BanerComponent } from './components/baner/baner.component';
 import { BrandSliderComponent } from './components/brand-slider/brand-slider.component';
 import { StyleSectionComponent } from './components/style-section/style-section.component';
+import { SharedModule } from '../shared/shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent, BanerComponent, BrandSliderComponent, StyleSectionComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
   exports: [HomeComponent]
 })
 export class HomeModule {}
