f8d041c47c25d981ed265d8578d0acab70d99096
patryk <patryk.pujanek@wp.pl>
2024-10-05 19:43:54 +0200

    added size filter and created function to handle price change

--- src/app/modules/product/components/products-page/products-page.component.ts
+++ src/app/modules/product/components/products-page/products-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { QueryParams } from '../../../core/models/params.model';
+import { min } from 'rxjs';
 
 @Component({
   selector: 'app-products-page',
@@ -12,6 +13,9 @@ export class ProductsPageComponent implements OnInit {
   isDescending: string = '';
   sortBy: string = '';
   dressStyle: string = '';
+  size: string = '';
+  minPrice: number = 0;
+  maxPrice: number = 2000;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -19,7 +23,7 @@ export class ProductsPageComponent implements OnInit {
     this.route.params.subscribe((params: QueryParams) => {
       if (params.category !== undefined) {
         this.category = params.category;
-        this.handleAddActive(this.category);
+        this.handleAddActive(this.category, 'active');
       }
       if (params.isDescending !== undefined) {
         this.isDescending = params.isDescending;
@@ -29,42 +33,65 @@ export class ProductsPageComponent implements OnInit {
       }
       if (params.dressStyle !== undefined) {
         this.dressStyle = params.dressStyle;
-        this.handleAddActive(this.dressStyle);
+        this.handleAddActive(this.dressStyle, 'active');
       }
     });
   }
 
-  handleAddActive(id: string): void {
+  handleAddActive(id: string, styleClass: string): void {
     const ele = document.getElementById(id);
     if (ele) {
-      ele.classList.add('active');
+      ele.classList.add(styleClass);
     }
   }
 
-  handleRemoveActive(id: string): void {
+  handleRemoveActive(id: string, styleClass: string): void {
     const ele = document.getElementById(id);
     if (ele) {
-      ele.classList.remove('active');
+      ele.classList.remove(styleClass);
     }
   }
 
   handleChangeCategory(event: Event) {
-    this.handleRemoveActive(this.category);
+    this.handleRemoveActive(this.category, 'active');
     const element = event.target as HTMLParagraphElement;
     let value = element.getAttribute('id');
     if (value) {
       this.category = value;
     }
-    this.handleAddActive(this.category);
+    this.handleAddActive(this.category, 'active');
   }
 
   handleChangeStyle(event: Event) {
-    this.handleRemoveActive(this.dressStyle);
+    this.handleRemoveActive(this.dressStyle, 'active');
     const element = event.target as HTMLParagraphElement;
     let value = element.getAttribute('id');
     if (value) {
       this.dressStyle = value;
     }
-    this.handleAddActive(this.dressStyle);
+    this.handleAddActive(this.dressStyle, 'active');
   }
+
+  handleChangeSize(event: Event) {
+    this.handleRemoveActive(this.size, 'active-size-item');
+    const element = event.target as HTMLParagraphElement;
+    let value = element.getAttribute('id');
+    if (value) {
+      this.size = value;
+    }
+    this.handleAddActive(this.size, 'active-size-item');
+  }
+
+  handleChangePrice(event: Event) {
+    const input = event.target as HTMLInputElement;
+    console.log(input);
+    if (input.name === 'min') {
+      this.minPrice = input.valueAsNumber;
+    }
+    if (input.name === 'max') {
+      this.maxPrice = input.valueAsNumber;
+    }
+  }
+
+  protected readonly min = min;
 }
